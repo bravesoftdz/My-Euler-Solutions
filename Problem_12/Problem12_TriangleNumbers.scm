30299bb426a3f4038dacd7f8d9769ea56a6c01c6
rpottsoh@gmail.com <Eclipse123>
2015-04-07 12:41:14 -0400

    Added to sub-folders

---
+++ Problem_12/Problem12_TriangleNumbers.scm
@@ -0,0 +1,45 @@
+;#lang racket
+#lang planet neil/sicp ;this line needed for DrRacket
+; comment both lines above this point to be MIT-Scheme compatible.
+
+(define (divides? n d)
+  (if (= 0 d)
+      #f
+      (= (remainder n d) 0)))
+
+(define (gettriangle n)
+  (/ (* n (+ n 1)) 2))
+
+
+
+(define (numsquare? n)
+  (= n (* (sqrt n) (sqrt n))))
+
+(define (numfactorsof n)
+  (define (factors-i d count)
+    (cond ((> d (sqrt n)) (if (numsquare? n) 
+                              (dec count)
+                              count))
+          ((divides? n d) (factors-i (inc d) (+ 2 count)))
+          (else (factors-i (inc d) count))))
+  (factors-i 1 0))
+
+(define (findfactorlimit seed x)
+  (define (finder-i i)
+    (let* ((t (gettriangle i))
+           (nft (numfactorsof t)))
+;      (display i)  
+;      (display " ")
+;      (display t)
+;      (display " ")
+;      (display nft)
+;      (display " ")
+;      (display x)
+;      (display "\n")
+      (if (>= nft x)
+          t
+          (finder-i (inc i)))))
+  (finder-i seed))
+
+;(findfactorlimit 12374 500)
+(findfactorlimit 1 500)
